aa0c8c1acfac97b7b36e66db78131424a9ca7218
Nougaga <54338876+Nougaga@users.noreply.github.com>
2020-07-29 18:29:01 +0900

    이미지 연습

--- 백준연습장/test.py
+++ 백준연습장/test.py
@@ -1,18 +1,9 @@
 import sys
 
 
-A = []
-B = []
 while True:
-    a,b = map(int,sys.stdin.readline().rstrip().split())
-    if (a==0 and b==0):
+    try:
+        a,b = map(int,sys.stdin.readline().rstrip().split())
+        print(a+b)
+    except:
         break
-    A.append(a)
-    B.append(b)
-
-i = 0
-length = len(A)
-while True:
-    print(A[i]+B[i])
-    
-
