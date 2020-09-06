6b0f51df6b614db83a569a707681d6a8644d6f99
Nougaga <54338876+Nougaga@users.noreply.github.com>
2020-09-06 12:40:16 +0900

    200902~200904-study

--- 백준연습장/test.py
+++ 백준연습장/test.py
@@ -1,36 +1,14 @@
 import sys
 
 
-N = int(sys.stdin.readline())
-res = N
+N, M = map(int, sys.stdin.readline().split())
+board = list()
+for i in range(N):
+    board.append(list(sys.stdin.readline().rstrip()))
 
 for i in range(N):
-    word = list(sys.stdin.readline().rstrip())
+    for j in range(M):
+        
 
-    j = 0
-    while(True):
-        if j >= len(word)-1:
-            break
 
-        if word[j] == word[j+1]:
-            del word[j+1]
-        else:
-            j += 1
 
-    length = len(word)
-    j = 0
-    while(True):
-        if j >= length:
-            break
-        k = j+1
-        while(True):
-            if k >= length:
-                break
-            if word[j] == word[k]:
-                res -= 1
-                j = length
-                break
-            k += 1
-        j += 1
-
-print(res)
