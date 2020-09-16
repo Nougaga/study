c244a2383735cc7d492ea859a9ba28153d0ff8b7
Nougaga <54338876+Nougaga@users.noreply.github.com>
2020-09-16 15:11:31 +0900

    upload

--- 백준연습장/test.py
+++ 백준연습장/test.py
@@ -1,19 +1,83 @@
 class Node:
     def __init__(self):
-        self.data=None
-        self.child=list()
+        self.value = ""
+        self.child = []
 
-    def setData(self,data:str):
-        self.data=data
+    def get_key(self):
+        return self.value
 
-    def getData(self):
-        return self.data
+    def get_child(self):
+        return self.child
 
-    def addChild(self,child:Node):
-        self.child.append(child)
-    
+    def add_child(self, string):
+        newNode = Node()
+        newNode.value = string
+        self.child.append(newNode)
 
-A = Node()
-B = A
 
-print(id(A)==id(B))
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def add_word(self, string, node=None):
+        if node == None:
+            node = self.root
+
+        depth = len(node.value)
+        lenString = len(string)
+        if depth == lenString:
+            print("'{}' is included.".format(string))
+            return 0
+
+        lenChild = len(node.child)
+        ptChild = 0
+        tempString = string[:depth+1]
+        while ptChild < lenChild:
+            is_found = node.child[ptChild].value == tempString
+            if is_found:
+                break
+            ptChild += 1
+        if ptChild >= lenChild:
+            node.add_child(tempString)
+        self.add_word(string, node=node.child[ptChild])
+
+    def count_child(self, node=None):
+        if node==None:
+            node=self.root
+        
+        if len(node.child) != 0:
+            res = len(node.child)
+            for i in range(len(node.child)):
+                res += self.count_child(node=node.child[i])
+        else:
+            res = 0
+
+        return res
+
+    def count_leaf(self, node=None):
+        if node==None:
+            node=self.root
+        if len(node.child) == 0:
+            if node != self.root:
+                return 1
+            else: return 0
+        else:
+            res = 0
+            for i in range(len(node.child)):
+                res += self.count_leaf(node=node.child[i])
+            return res
+
+
+TrieTree = Trie()
+# TrieTree.add_word("word")
+# TrieTree.add_word("work")
+# TrieTree.add_word("word")
+# TrieTree.add_word("word")
+# TrieTree.add_word("work")
+# TrieTree.add_word("wore")
+print(TrieTree.count_child())
+print(TrieTree.count_leaf())
+
+
+
+
