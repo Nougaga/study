565646f3d99237872428f658af21537f2c155f6f
Nougaga <54338876+Nougaga@users.noreply.github.com>
2020-10-13 21:13:07 +0900

    ~2020-10-13 study

--- 연습장/py/test.py
+++ 연습장/py/test.py
@@ -1,23 +1,6 @@
-from heapq import heapify, heappush, heappop
+import heapq
+import keras
 
-def solution(scoville, K):
-    answer = 0
-    heapify(scoville)
-    while True:
-        temp = scoville[0]
-        if temp >= K:
-            break 
-        if len(scoville) <= 1:
-            answer = -1
-            break
-        heappush(scoville, heappop(scoville) + heappop(scoville)*2)
-        answer += 1
-    return answer
 
 if __name__ == "__main__":
-    scoville = [1, 2, 3, 9, 10, 12]
-    K = 7
-
-    print(solution(scoville, K))
-
-    
+    pass
