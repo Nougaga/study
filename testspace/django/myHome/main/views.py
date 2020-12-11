edee701ef4d5d98519ab03110fff51c31072d8a8
Nougaga <zmni123@naver.com>
2020-12-12 03:40:45 +0900

    장고 복습

--- testspace/django/myHome/main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def main(request):
-    return render(request, 'main/main.html', {})
-
-
-def test(request):
-    return render(request, 'main/temp.html', {})
