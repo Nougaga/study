adab95225da68d0378fd88db9ec78c5580d6aaa5
Nougaga <zmni123@naver.com>
2020-12-13 23:09:37 +0900

    와! 신나는 장고 공부!

--- testspace/django/test1/polls/admin.py
+++ testspace/django/test1/polls/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text'], }),
+        ('Date Information', {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]    # Choice 모델을 같이 볼 수 있음
+    list_display = ('question_text', 'pub_date')    # 리스트에 보여줄 속성들
+    list_filter = ['pub_date']  # pub_date로 필터 기능
+    search_fields = ['question_text']   # question_text 검색 기능
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
